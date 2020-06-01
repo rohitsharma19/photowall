516f236b89dd5c9f6f56a2b145008f4a222aae66
Rohit Sharma <rohitsharma19@gmail.com>
2020-06-01 20:16:56 +0530

    changes to deploy on gh-pages

---
+++ build/precache-manifest.10111661e3f309512ec7c740f69bcc57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95d444b421fa20d259cb5e92a1e7f1d1",
+    "url": "/photowall/index.html"
+  },
+  {
+    "revision": "504f82e0ef0fed7d8ca2",
+    "url": "/photowall/static/css/main.96827eda.chunk.css"
+  },
+  {
+    "revision": "3e042a4de2f7498a05dd",
+    "url": "/photowall/static/js/2.6c4be1ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/photowall/static/js/2.6c4be1ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "504f82e0ef0fed7d8ca2",
+    "url": "/photowall/static/js/main.47b194a5.chunk.js"
+  },
+  {
+    "revision": "4a4c8d5390363ffb90dd",
+    "url": "/photowall/static/js/runtime-main.e1920836.js"
+  }
+]);
